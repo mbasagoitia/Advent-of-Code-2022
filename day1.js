4ed07eb0c36806237f53c2db533779ea76ff81c7
basagoitiam <marika.basagoitia@gmail.com>
2022-12-05 10:19:17 -0700

    fixed day 1 solution

--- day1.js
+++ day1.js
@@ -1,2 +1,32 @@
-const data = readFileSync('day1_data.txt', 'utf8');
-console.log(data);
+//part 1
+
+fetch("https://adventofcode.com/2022/day/1/input").
+then((res) => res.text())
+.then((data) => {
+    let nums = data.split("\n\n");
+    let arr = nums.map((data) => data.split("\n"));
+    let sumArr = arr.map((el) => {
+        return el.reduce((acc, number) => {
+            return parseInt(number) + parseInt(acc);
+        }, 0);
+    })
+    sumArr.pop();
+    //elf with MOST
+    console.log(Math.max(...sumArr));
+    //TOP THREE
+    let topThreeArr = [];
+    topThreeArr.push(Math.max(...sumArr));
+    let mostCalsIndex = sumArr.indexOf(Math.max(...sumArr));
+    removeFromArr(sumArr, mostCalsIndex);
+    topThreeArr.push(Math.max(...sumArr));
+    let secondMostCalsIndex = sumArr.indexOf(Math.max(...sumArr));
+    removeFromArr(sumArr, secondMostCalsIndex);
+    topThreeArr.push(Math.max(...sumArr));
+    
+    console.log(topThreeArr.reduce((acc, num) => (acc + num), 0));
+})
+.catch((err) => console.error(err));
+
+function removeFromArr(arr, targetIndex) {
+    arr.splice(targetIndex, 1);
+}
